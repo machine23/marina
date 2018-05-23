0046f72cde73eac4aa873448fde0ad3893e1862d
Vassili Baranov <vassili.baranov@gmail.com>
2018-05-23 11:10:12 +0300

    Display questions count at IndexView page

--- quiz/tests/test_views.py
+++ quiz/tests/test_views.py
@@ -1,4 +1,8 @@
 from django.urls import reverse
+from mixer.backend.django import mixer
+import pytest
+
+pytestmark = pytest.mark.django_db
 
 
 class TestIndexView:
@@ -15,3 +19,9 @@ class TestIndexView:
         used_templates = [template.name for template in resp.templates]
         assert 'quiz/index.html' in used_templates
         assert 'Hello from Marina' in resp.content.decode()
+
+    def test_index_view_displays_questions_count(self, client):
+        mixer.cycle(5).blend('quiz.Question')
+        resp = client.get(reverse('quiz:index'))
+        assert resp.status_code == 200
+        assert resp.context['questions_count'] == 5
--- quiz/views.py
+++ quiz/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
 from django.views.generic import TemplateView
+from quiz.models import Question
 
 
 class IndexView(TemplateView):
     template_name = 'quiz/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['questions_count'] = Question.objects.count()
+        return context
