78c19c23881ce292f68db2a5f2cbcedea672f251
Vassili Baranov <vassili.baranov@gmail.com>
2018-06-11 22:12:37 +0300

    Create QuestionTheme model

--- quiz/models.py
+++ quiz/models.py
@@ -1,7 +1,15 @@
 from django.db import models
 
 
+class QuestionTheme(models.Model):
+    title = models.CharField(max_length=75)
+
+    def __str__(self):
+        return self.title
+
+
 class Question(models.Model):
+    theme = models.ForeignKey(QuestionTheme, on_delete=models.CASCADE, blank=True, null=True)
     text = models.TextField('Question')
     answer = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
--- quiz/tests/test_models.py
+++ quiz/tests/test_models.py
@@ -1,9 +1,28 @@
 import pytest
 from mixer.backend.django import mixer
 
-from quiz.models import Question
+from quiz.models import Question, QuestionTheme
 
 pytestmark = pytest.mark.django_db
+
+
+class TestQuestionThemeModel:
+    def test_saving_and_retrieving_theme(self):
+        mixer.blend(QuestionTheme, title='question test theme')
+        themes = QuestionTheme.objects.all()
+        assert themes.count() == 1
+        assert themes[0].title == 'question test theme'
+
+    def test_title_max_length(self):
+        mixer.blend(QuestionTheme)
+        theme = QuestionTheme.objects.first()
+        max_length = theme._meta.get_field('title').max_length
+        assert max_length == 75
+
+    def test_string_representation(self):
+        mixer.blend(QuestionTheme, title='question theme')
+        theme = QuestionTheme.objects.first()
+        assert str(theme) == 'question theme'
 
 
 class TestQuestionModel:
