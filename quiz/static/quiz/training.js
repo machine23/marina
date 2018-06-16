a6074a1ac6431e7352d605ff2e7e93853d1be6fc
Vassili Baranov <vassili.baranov@gmail.com>
2018-06-16 22:03:53 +0300

    Change checkAnswer

--- quiz/static/quiz/training.js
+++ quiz/static/quiz/training.js
@@ -1,32 +1,3 @@
-// window.onload = getQuestion;
-// let form = document.querySelector('#quiz_form');
-// let skip_btn = document.querySelector('#skip');
-// skip_btn.onclick = getQuestion;
-// let answer = '';
-// form.onsubmit = checkAnswer;
-// function getQuestion(e) {
-//     fetch(URL, {
-//         credentials: 'include',
-//     })
-//         .then(response => response.json())
-//         .then(json => {
-//             console.log(json)
-//             document.querySelector('#question').innerHTML = json.question;
-//             document.querySelector('#answer').value = '';
-//             document.querySelector('#id_answer').value = '';
-//             answer = json.answer.trim().replace('\r\n', '\n');
-//         })
-// }
-// function checkAnswer(e) {
-//     e.preventDefault();
-//     user_answer = document.querySelector('#id_answer').value;
-//     if (user_answer == answer) {
-//         getQuestion();
-//     } else {
-//         document.querySelector('#answer').value = answer;
-//     }
-// }
-
 new Vue({
     el: '#training-app',
     delimiters: ['${', '}'],
@@ -46,6 +17,7 @@ new Vue({
 
         getQuestion() {
             this.userAnswer = '';
+            this.answerIsVisible = false;
 
             fetch(URL, {
                     credentials: 'include',
@@ -60,9 +32,10 @@ new Vue({
         },
 
         checkAnswer() {
-            console.log(this.userAnswer);
-            console.log((this.userAnswer == this.answer))
-            if (this.userAnswer.trim() == this.answer) {
+            userAnswer = this.userAnswer.replace(/\s+/g, ' ').trim();
+            answer = this.answer.replace(/\s+/g, ' ').trim();
+
+            if (userAnswer == answer) {
                 this.getQuestion();
             }
         },
